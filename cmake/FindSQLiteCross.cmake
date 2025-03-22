02f74e822536b0db23c5b7c6bcce7eb8dcd955c7
Dracovian <dracoviandev@protonmail.com>
2025-03-21 20:11:07 -0500

    Attempt to find SQLite libraries in Windows by manually setting the chocolatey package path.

--- cmake/FindSQLiteCross.cmake
+++ cmake/FindSQLiteCross.cmake
@@ -1,9 +1,12 @@
-find_library(SQLiteCross_LIBRARY NAMES sqlite3)
-mark_as_advanced(SQLiteCross_LIBRARY)
+if(WIN32)
+    find_library(SQLiteCross_LIBRARY NAMES sqlite3 PATHS "C:\\ProgramData\\chocolatey\\lib\\SQLite")
+    find_path(SQLiteCross_INCLUDE_DIR NAMSE sqlite3.h PATHS "C:\\ProgramData\\chocolatey\\lib\\SQLite")
+else()
+    find_library(SQLiteCross_LIBRARY NAMES sqlite3)
+    find_path(SQLiteCross_INCLUDE_DIR NAMES sqlite3.h)
+endif()
 
-find_path(SQLiteCross_INCLUDE_DIR NAMES sqlite3.h)
-mark_as_advanced(SQLiteCross_INCLUDE_DIR)
-
+mark_as_advanced(SQLiteCross_LIBRARY SQLiteCross_INCLUDE_DIR)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SQLiteCross 
     DEFAULT_MSG SQLiteCross_LIBRARY SQLiteCross_INCLUDE_DIR)
